d1fe965fa4e6754984286d7179785ee64fa0be9d
Prasanjit dutta <57594105+prasanjit101@users.noreply.github.com>
2020-10-03 20:09:47 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar from './Navbar'
+import {Navbar,Nav} from 'react-bootstrap';
+import GoogleBtn from './Login'
 import Main from './Main';
+import {auth} from '../firebase';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Main/>
-      </div>
-    );
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
   }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+}
+
+function App() {
+  var user = auth.currentUser;
+  return (
+	{
+<>
+	<div>
+        <Navbar sticky="top" bg="dark" id="navbar" variant="dark">
+      <Navbar.Brand href="/"><img alt="logo" src="../logo.svg" width="70px"/> Manthan<span>.</span></Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+      <Nav>
+      <Nav.Link id="nav-link" href="/"><span>Create</span></Nav.Link>
+      <GoogleBtn/>
+      </Nav>
+      </Navbar.Collapse>
+      </Navbar>
+	  <Main name = "user.displayName" email = "user.email" photoUrl = "user.photoURL" />
+      </div>
+	  </>
+	   }
+      
+    );
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Dropdown} from 'react-bootstrap';
+import {auth} from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+function GoogleBtn(){
+	
+	const [user] = useAuthState(auth);
+	const { uid, photoURL } = auth.currentUser;
+	const signInWithGoogle = () => {
+    const provider = new auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+    return (
+    <div>
+      { user ?
+      <>
+      <Dropdown alignRight>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  <img className="dropbtn" alt="avtar" src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} width="50px"/>
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item>{user.displayName}</Dropdown.Item>
+    <Dropdown.Item>{user.email}</Dropdown.Item>
+    <Dropdown.Item>{uid}</Dropdown.Item>
+    <Dropdown.Item>
+		<button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+	</Dropdown.Item>
+    
+  </Dropdown.Menu>
+</Dropdown>
+        </>: <button className="sign-in" onClick={signInWithGoogle}>Sign in</button>
+      }
+    </div>
+    )
+}
+
+export default GoogleBtn;
